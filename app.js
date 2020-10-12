48d72e43174c29fe60fd08cd065902b03e30266d
Victor Adewumi <VictorOluwaseun@users.noreply.github.com>
2020-10-12 22:56:26 +0100

    Ready for deployment

--- app.js
+++ app.js
@@ -15,7 +15,19 @@ const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
+app.enable('trust proxy');
+
 // 1) GLOBAL MIDDLEWARES
+
+app.use(cors());
+// Access-Control-Allow-Origin *
+// api.natours.com, front-end natours.com
+// app.use(cors({
+//   origin: 'https://www.natours.com'
+// }))
+
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
