b534dd24dd7d8b37d38e85a64ab1212400a1d5d8
Victor Adewumi <VictorOluwaseun@users.noreply.github.com>
2020-10-13 20:04:55 +0100

    Fixed bugs

--- models/userModel.js
+++ models/userModel.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Please provide a password'],
-    minlength: [8, 'Password must be more than 7 characters'],
+    minlength: 8,
     select: false
   },
   passwordConfirm: {
