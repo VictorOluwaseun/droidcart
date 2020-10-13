833e1d43416f41bac65903298cda8aec5293deba
Victor Adewumi <VictorOluwaseun@users.noreply.github.com>
2020-10-13 03:26:46 +0100

    Added documentation to the homepage

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.use((req, res, next) => {
 // 3) ROUTES
 
 app.get("/", (req, res, next) => {
-  res.send("<h1>Welcome to DroidCart</h1>")
+  res.sendFile("./public/index.html")
 })
 
 app.use("/api/v1/users", userRouter)
